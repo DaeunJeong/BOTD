312d2653da4461b04a28cfee4858821c8b40ba1c
Dani <alal8272@gmail.com>
2025-01-06 22:03:16 +0900

    프로젝트 생성.

---
+++ BOTD/BOTD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BOTD
+//
+//  Created by 정다은 on 1/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BOTD/BOTDTests/BOTDTests.swift
@@ -0,0 +1,17 @@
+//
+//  BOTDTests.swift
+//  BOTDTests
+//
+//  Created by 정다은 on 1/6/25.
+//
+
+import Testing
+@testable import BOTD
+
+struct BOTDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
